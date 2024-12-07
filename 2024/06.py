a7bee8891e36fd76a92a670d7b1b2da6809175c9
nileshwar-ganesh <nileshwar.gk@gmail.com>
2024-12-07 02:47:29 +0100

    upload: day 6 complete solution (brute force)

--- 2024/06.py
+++ 2024/06.py
@@ -1,4 +1,5 @@
 import copy
+import sys
 
 with open("data") as file:
     content = file.readlines()
@@ -51,78 +52,38 @@ while True:
                 break
 
 print(len(set(position_cp)))
-
 loop_positions = []
-for r, _ in enumerate(path_matrix):
-    print("Current row = ", r)
-    for c, val in enumerate(path_matrix[0]):
-        r = 17
-        c = 7
-        path_matrix_cp = copy.deepcopy(path_matrix)
-        if val != '#':
-            path_matrix_cp[r][c] = '#'
-            for i in range(4):
-                position_cp = []
-                direction_cp = []
-                if i == 0:
-                    # top left check
-                    if 0 <= r+1 < len(path_matrix):
-                        if path_matrix_cp[r+1][c] == '#':
-                            continue
-                    position_cp.append((r + 1, c))
-                    direction_cp.append((0, 1))
-                elif i == 1:
-                    # top right check
-                    if 0 <= c - 1 < len(path_matrix[0]):
-                        if path_matrix_cp[r][c - 1] == '#':
-                            continue
-                    position_cp.append((r, c - 1))
-                    direction_cp.append((1, 0))
-                elif i == 2:
-                    # bot right check
-                    if 0 <= r - 1 < len(path_matrix):
-                        if path_matrix_cp[r - 1][c] == '#':
-                            continue
-                    position_cp.append((r - 1, c))
-                    direction_cp.append((0, -1))
-                elif i == 3:
-                    # bot left check
-                    if 0 <= c + 1 < len(path_matrix[0]):
-                        if path_matrix_cp[r][c + 1] == '#':
-                            continue
-                    position_cp.append((r, c + 1))
+positions = set(position_cp)
+path_matrix_cp = copy.deepcopy(path_matrix)
+position_cp = copy.deepcopy(position)
+direction_cp = copy.deepcopy(direction)
+
+for idx, pos in enumerate(positions):
+    if idx % 100 == 0:
+        print(idx)
+    path_matrix_cp = copy.deepcopy(path_matrix)
+    position_cp = copy.deepcopy(position)
+    direction_cp = copy.deepcopy(direction)
+    path_matrix_cp[pos[0]][pos[1]] = '#'
+    while True:
+        row = position_cp[-1][0] + direction_cp[-1][0]
+        col = position_cp[-1][1] + direction_cp[-1][1]
+        if 0 <= row < len(path_matrix_cp) and 0 <= col < len(path_matrix_cp[0]):
+            if path_matrix_cp[row][col] == '#':
+                if direction_cp[-1] == (0, -1):
                     direction_cp.append((-1, 0))
+                elif direction_cp[-1] == (-1, 0):
+                    direction_cp.append((0, 1))
+                elif direction_cp[-1] == (0, 1):
+                    direction_cp.append((1, 0))
+                elif direction_cp[-1] == (1, 0):
+                    direction_cp.append((0, -1))
+            else:
+                if position_cp.count((row, col)) > 4:
+                    loop_positions.append(pos)
+                    break
+                position_cp.append((row, col))
+                if row == 0 or row == len(path_matrix_cp) - 1 or col == 0 or col == len(path_matrix_cp[0]) - 1:
+                    break
 
-                print(position_cp, direction_cp)
-
-                while True:
-                    row = position_cp[-1][0] + direction_cp[-1][0]
-                    col = position_cp[-1][1] + direction_cp[-1][1]
-                    if 0 <= row < len(path_matrix_cp) and 0 <= col < len(path_matrix_cp[0]):
-                        if path_matrix_cp[row][col] == '#':
-                            print("here")
-                            if direction_cp[-1] == (0, -1):
-                                direction_cp.append((-1, 0))
-                            elif direction_cp[-1] == (-1, 0):
-                                direction_cp.append((0, 1))
-                            elif direction_cp[-1] == (0, 1):
-                                direction_cp.append((1, 0))
-                            elif direction_cp[-1] == (1, 0):
-                                direction_cp.append((0, -1))
-                        else:
-                            print(row, col)
-                            if direction_cp[-1] == (-1, 0) and col != c:
-                                break
-                            if position_cp.count((row, col)) > 4:
-                                loop_positions.append((r, c))
-                                break
-                            position_cp.append((row, col))
-                            if row <= 0 or row >= len(path_matrix_cp) - 1 or col <= 0 or col >= len(path_matrix_cp[0]) - 1:
-#                                print("valid exit")
-                                break
-                    else:
-                        break
-#                print("it did break")
-#        break
-#    break
-print(len(set(loop_positions)))
+print(len(loop_positions))
