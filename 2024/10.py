6ae095408dbc99ce65da9e012caa97f5d59a09a0
nileshwar-ganesh <nileshwar.gk@gmail.com>
2024-12-12 13:50:17 +0100

    upload: day 10 complete solution (recursion)

--- 2024/10.py
+++ 2024/10.py
@@ -1,23 +1,24 @@
-def find_peak(value, position):
-    for pos in position:
-        row = pos[0]
-        col = pos[1]
-        direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        found = []
-        for d in direction:
-            if 0 <= row + d[0] < len(matrix) and 0 <= col + d[1] < len(matrix[0]):
-                if matrix[row + d[0]][col + d[1]] == value + 1:
-                    r = row + d[0]
-                    c = col + d[1]
-                    found.append((r, c))
-        if len(found) >= 1 and value < 9:
-            for entry in found:
-                find_peak(value + 1, entry)
-        else:
-            return False
+# recursive function which calculates trail to the peak
+def find_peak(value, row, col):
+    # using a global variable counter to hold the positions of peak (value = 9)
+    global counter
+    # the direction is either one step left or right / top or bottom
+    direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    # if we reach a peak, we need to store that (row, col)
+    if value == 9:
+        counter.append((row, col))
+        return True
+    # else, look for the next higher number in the boundary cell
+    for d in direction:
+        r = row + d[0]
+        c = col + d[1]
+        if 0 <= row + d[0] < len(matrix) and 0 <= col + d[1] < len(matrix[0]):
+            # if the value matches, recursively call the function
+            if matrix[row + d[0]][col + d[1]] == value + 1:
+                find_peak(value + 1, r, c)
 
-
-with open("example") as file:
+# read from data file
+with open("data") as file:
     content = file.readlines()
 
 # creating a readable matrix out of input data
@@ -25,14 +26,16 @@ matrix = []
 for line in content:
     matrix.append([int(char) for char in line.strip()])
 
-valley_positions = []
+total_unique_trailheads = 0
+total_unique_paths = 0
 for row, entry in enumerate(matrix):
-    for col, value in enumerate(matrix[0]):
+    for col, val in enumerate(matrix[0]):
+        counter = []
         if matrix[row][col] == 0:
-            valley_positions.append((row, col))
+            find_peak( 0, row, col)
+            total_unique_trailheads += len(set(counter))
+            total_unique_paths += len(counter)
+print("Part 1. Total unique trailheads from every 0 position = ", total_unique_trailheads)
+print("Part 2. Total unique paths to trailheads from every 0 position = ", total_unique_paths)
 
-for position in valley_positions:
-    print(find_peak(0, [position]))
-
-print(valley_positions)
 
