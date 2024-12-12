ca2df8f9cbbbddb890492166cbada2d9ad489601
nileshwar-ganesh <nileshwar.gk@gmail.com>
2024-12-12 18:22:22 +0100

    upload: day 12 partial solution (part 1)

--- 2024/12.py
+++ 2024/12.py
@@ -11,10 +11,20 @@ def find_plot(value, row, col):
                 if (r, c) not in visited_positions:
                     visited_positions.append((r, c))
 
+def find_perimeter(plot):
+    global plot_positions
+    perimeter = 0
+    direction = [(-1, 0), (1, 0), (0, 1), (0, -1) ]
+    for d in direction:
+        r = plot[0] + d[0]
+        c = plot[1] + d[1]
+        if (r, c) not in plot_positions:
+            perimeter += 1
+    return perimeter
 
 
 # read from data file
-with open("example") as file:
+with open("data") as file:
     content = file.readlines()
 
 matrix = []
@@ -22,9 +32,17 @@ for line in content:
     matrix.append([char for char in line.strip()])
 
 visited_positions = []
+fencing = 0
 for row, entries in enumerate(matrix):
     for col, value in enumerate(entries):
         if (row, col) not in visited_positions:
             plot_positions = [(row, col)]
             find_plot(matrix[row][col], row, col)
-            print(plot_positions)
+            area = len(plot_positions)
+            perimeter = 0
+            for plot in sorted(plot_positions):
+                perimeter += find_perimeter(plot)
+            fencing += area * perimeter
+            print(matrix[row][col], area, perimeter, fencing)
+
+print(fencing)
