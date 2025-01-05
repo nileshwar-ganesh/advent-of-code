6a977cd39397b3737a3b9cc95eed8ce50f8a141b
nileshwar-ganesh <nileshwar.gk@gmail.com>
2025-01-05 15:41:14 +0100

    upload: day 1 complete solution (with comments)

--- 2023/01.py
+++ 2023/01.py
@@ -4,6 +4,9 @@ with open("data") as file:
     content = file.readlines()
 
 def find_calibration_values(data, is_part_2=False):
+    # creating a number map, because the numbers may share a letter
+    # e.g. oneight, where 'e' is shared by one and eight
+    # a simple replace will either make it on8 or 1ight
     number_map = {'one': 'o1e', 'two': 't2o', 'three': 't3e',
                   'four': 'f4r', 'five': 'f5e', 'six': 's6x',
                   'seven': 's7n', 'eight': 'e8t', 'nine': 'n9e'}
@@ -11,6 +14,7 @@ def find_calibration_values(data, is_part_2=False):
     for line in data:
         line = line.strip()
         if is_part_2:
+            # for part two, we also consider numbers which are spelled
             for key, value in number_map.items():
                 line = re.sub(key, value, line)
         numbers = re.findall(r"\d", line)
