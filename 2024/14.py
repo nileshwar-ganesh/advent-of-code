5ddbf8b9ac2a22670437ed100574a74a97abde29
nileshwar-ganesh <nileshwar.gk@gmail.com>
2025-01-23 17:07:04 +0100

    upload: day 14 complete solution (reworked)

--- 2024/14.py
+++ 2024/14.py
@@ -1,11 +1,13 @@
+import copy
 import re
 
-def navigate(position, velocity, steps):
-    global final_positions
-    if steps == 0:
-        final_positions.append(position)
-        return position
-    else:
+def navigate_step():
+    top_left = 0
+    top_right = 0
+    bot_left = 0
+    bot_right = 0
+    for robot in robots:
+        position, velocity = robot[0], robot[1]
         r = position[1] + velocity[1]
         c = position[0] + velocity[0]
         if r < 0:
@@ -16,7 +18,19 @@ def navigate(position, velocity, steps):
             c += tile_width
         if c >= tile_width:
             c = abs(c % tile_width)
-        navigate((c, r), velocity, steps - 1)
+
+        if 0 <= r < (tile_height // 2) and 0 <= c < (tile_width // 2):
+            top_left += 1
+        if (tile_height // 2) < r < tile_height and 0 <= c < (tile_width // 2):
+            bot_left += 1
+        if 0 <= r < (tile_height // 2) and (tile_width // 2) < c < tile_width:
+            top_right += 1
+        if (tile_height // 2) < r < tile_height and (tile_width // 2) < c < tile_width:
+            bot_right += 1
+
+        robot[0] = (c, r)
+
+    return top_left * top_right * bot_left * bot_right
 
 
 with open("data") as file:
@@ -31,23 +45,11 @@ for line in content:
     values = re.findall(r"-?\d+", line)
     robots.append([(int(values[0]), int(values[1])), (int(values[2]), int(values[3]))])
 
-for robot in robots:
-    navigate(robot[0], robot[1], 100)
+scores = []
+for step in range(1, 10000):
+    score = navigate_step()
+    scores.append(score)
+    if step == 100:
+        print('Part 1. The safety factor after 100 seconds = ', score)
 
-top_left = 0
-top_right = 0
-bot_left = 0
-bot_right = 0
-for position in final_positions:
-    if 0 <= position[1] < (tile_height // 2) and 0 <= position[0] < (tile_width // 2):
-        top_left += 1
-    if (tile_height // 2) < position[1] < tile_height and 0 <= position[0] < (tile_width // 2):
-        bot_left += 1
-    if 0 <= position[1] < (tile_height // 2) and (tile_width // 2) < position[0] < tile_width:
-        top_right += 1
-    if (tile_height // 2) < position[1] < tile_height and (tile_width // 2) < position[0] < tile_width:
-        bot_right += 1
-
-
-print(top_left, top_right, bot_left, bot_right)
-print(top_left * top_right * bot_left * bot_right)
+print('Part 2. Time taken to display easter egg = ', scores.index(min(scores)) + 1)
